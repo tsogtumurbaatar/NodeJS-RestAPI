c8c831dfc39e08bb258e6d800fe7b63a9740515f
DOMAIN\tts <tts@mspgroup.com.au>
2017-11-02 15:35:22 +1030

    Backend REST API

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://admin:password@ds143245.mlab.com:43245/bookstoretsog');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
